5e156381b81df3773bf2132c9d343d4de3f44fde
Frédéric Gessler <36667834+gessfred@users.noreply.github.com>
2022-11-21 13:21:08 +0000

    [KEY-1] Implement basic tests

---
+++ test_app.py
@@ -0,0 +1,27 @@
+from uuid import uuid4
+from datetime import datetime
+from app import app, database
+from fastapi.testclient import TestClient 
+from unittest.mock import MagicMock
+client = TestClient(app)
+
+def mock_db():
+    return MagicMock()
+
+app.dependency_overrides[database] = mock_db
+
+
+def test_events_crud():
+    test_request = [
+        {
+            "record_time": str(datetime.now()),
+            "session_id": uuid4().hex,
+            "source_url": "google.com",
+            "is_end_of_word": False,
+            "is_end_of_line": False
+        }
+    ]
+    res = client.post("/events", json={"events": test_request})
+    assert res.status_code == 200
+    res = client.get("/events")
+    assert res.status_code == 200
