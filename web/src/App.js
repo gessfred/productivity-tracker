a471292064e5e75818e88ec37a5d3ebf5aebc8ef
Frederic Gessler <gessfred@protonmail.com>
2023-07-09 01:42:16 +0200

    Generate landing page static background

--- web/src/App.js
+++ web/src/App.js
@@ -1,21 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
-import { Auth0Provider, useAuth0 } from '@auth0/auth0-react'
-import { auth0Ctx, ctx } from './contexts'
+import { useState, useEffect } from 'react'
+import './App.css'
+import { useAuth0 } from '@auth0/auth0-react'
 
-const LoginButton = () => {
-  const { loginWithRedirect, isAuthenticated } = useAuth0();
-  console.log(isAuthenticated)
-  return <button onClick={() => loginWithRedirect()}>Log In</button>;
+
+//TODO make moving SVG background
+//TODO Add blurry foreground  
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
+}
+
+function getWindowDimensions() {
+  const { innerWidth: width, innerHeight: height } = window;
+  return {
+      width,
+      height
+  };
+}
+function useWindowDimensions() {
+  const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
+  useEffect(() => {
+      function handleResize() {
+          setWindowDimensions(getWindowDimensions());
+      }
+      window.addEventListener('resize', handleResize);
+      return () => window.removeEventListener('resize', handleResize);
+  }, []);
+  return windowDimensions;
+}
+
+function BlurOverlay({children}) {
+  return (
+    <div className='blur-overlay'>
+      {children}
+    </div>
+  )
+}
+
+function LandingBackground({}) {
+  const { width, height } = useWindowDimensions()
+  const colors = ['#C0B8DF', '#F9FB95', '#FBBC95']
+  return (
+    <svg className='background' xmlns="http://www.w3.org/2000/svg">
+      {[...Array(10).keys()].map(i => (
+        <circle 
+          cx={getRandomInt(0, width)} 
+          cy={getRandomInt(0, height)} 
+          r={getRandomInt(50, 200)} 
+          fill={colors[getRandomInt(0, 4)]} />
+      ))}
+    </svg>
+  )
+}
+
+function LandingPage({show}) {
+  const { loginWithRedirect, isAuthenticated } = useAuth0()
+  return (
+    <div id='landing-page'>
+      
+      <LandingBackground />
+      <BlurOverlay>
+        <span>Keylogg</span>
+        <span>Get the best from yourself</span>
+        <button onClick={() => loginWithRedirect()}>Log In</button>
+      </BlurOverlay>
+    </div>
+  )
 }
 
 function App() {
+  const { loginWithRedirect, isAuthenticated } = useAuth0()
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <LoginButton />
-      </header>
+        <LandingPage />
+
     </div>
   );
 }
