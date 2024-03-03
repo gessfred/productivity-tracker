98660ccaf34aa69688025acf1d2b88ae41f1449f
Frédéric Gessler <gessfred@protonmail.com>
2024-03-03 21:24:01 +0100

    create skeleton for desktop app

---
+++ app/src/renderer/App.tsx
@@ -0,0 +1,27 @@
+import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
+import icon from '../../assets/icon.svg';
+import './App.css';
+
+function Hello() {
+  return (
+    <div>
+      <div className="Hello">
+        <img width="200" alt="icon" src={icon} />
+      </div>
+      <h1>Dashboard</h1>
+      <div className="Hello">
+        
+      </div>
+    </div>
+  );
+}
+
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Hello />} />
+      </Routes>
+    </Router>
+  );
+}
